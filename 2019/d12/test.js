0c8c3ffcc0389a8a34f3c7972709b5a5ec1ad138
Osman Zubair <ozubair@deloitte.co.uk>
2019-12-22 13:22:10 +0000

    d12

---
+++ 2019/d12/test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+// const sensorboost = require('./sensorboost');
+
+
+// describe('Day 9', () => {
+//     it('Run Sensor Boost Sample 1', () => {
+//         instructions = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99];
+//         inputs = []
+//         output = instructions.slice()
+//         assert.deepEqual(sensorboost(instructions, inputs), output)
+//     });
+
+// })
