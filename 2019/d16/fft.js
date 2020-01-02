01528195619c6bddd4ff44676b168ce188848a72
Osman Zubair <ozubair@deloitte.co.uk>
2020-01-02 08:43:47 +0000

    d16

--- 2019/d16/fft.js
+++ 2019/d16/fft.js
@@ -6,7 +6,7 @@ run_phase = (input, base_pattern, offset = 0) => {
     for (let i = 0; i < input.length; i ++){
         total = 0
         for (let j = input.length-1; j >= offset; j--){
-            base_pattern_index = (Math.floor( ((j+1) / (i+1)) )) % base_pattern.length
+            base_pattern_index = (Math.floor( ((offset+j+1) / (i+1)) )) % base_pattern.length
             // console.log(i, j, base_pattern_index, input[j], base_pattern[base_pattern_index])
             output = Math.abs(input[j] * base_pattern[base_pattern_index]) % 10
             total += input[j] * base_pattern[base_pattern_index]
@@ -17,34 +17,37 @@ run_phase = (input, base_pattern, offset = 0) => {
     return new_number.join('')
 }
 
-run_phases_n = (input, base_pattern, n, offset_message = false, repeat = false) => {
+run_phases_n = (input, base_pattern, n) => {
     let offset = 0
-    if (repeat){
-        input = input.repeat(10000)
-    }
-    console.log(input.length)
-    if (offset_message === true){
-        offset = parseInt(input.slice(0, 7))
-        // input = input.slice(offset)
-        // console.log(offset, input.length)
-    }
 
     for (let i = 0; i<n; i++){
         input = input.padStart(8, '0').split('').map(x => parseInt(x))
         input = run_phase(input, base_pattern, offset)
-        // console.log(input,i,  n, i<n)
-        console.log(i)
     }
     return input.slice(0, 8)
 }
 
-module.exports = run_phases_n
+part2 = input => {
+    let offset = parseInt(input.slice(0,7))
+    input = input.slice().repeat(10000).split('').map(x => parseInt(x))
+    let total = 0
+    for (let i =0; i<100; i++){
+        total = 0
+        for (let idx = input.length-1; idx >= offset; idx--){
+            total += parseInt(input[idx])
+            input[idx] = Math.abs(total)%10
+        }
+    }
+    return input.slice(offset, offset+8).join('')
+}
+
+module.exports = {fft: run_phases_n, part2: part2}
 
 if (!module.parent) {
     let fs = require('fs')
     data = Buffer.from(fs.readFileSync('d16.txt')).toString()
-    console.log(run_phases_n(data, [0, 1, 0, -1], 100))
+    console.log(run_phases_n(data.slice(), [0, 1, 0, -1], 100))
 
-    console.log(run_phases_n("03036732577212944063491565474664", [0, 1, 0, -1], 100, true, true))
+    console.log(part2(data.slice()))
 
   }
--- 2019/d16/test.js
+++ 2019/d16/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
-const fft = require('./fft');
-
+const fftlib = require('./fft');
+const fft = fftlib.fft
+const part2 = fftlib.part2
 
 describe('Day 16', () => {
     it('Run Phases Sample 1', () => {
@@ -35,27 +36,27 @@ describe('Day 16', () => {
         assert.strictEqual(fft(input, base_pattern, n), expected)
     });
 
-    // it('Run Phases Repeat Sample 1', () => {
-    //     input = "03036732577212944063491565474664"
-    //     base_pattern = [0, 1, 0, -1]
-    //     n = 100
-    //     expected = "84462026"
-    //     assert.strictEqual(fft(input, base_pattern, n, true, true), expected)
-    // });
+    it('Run Phases Repeat Sample 1', () => {
+        input = "03036732577212944063491565474664"
+        base_pattern = [0, 1, 0, -1]
+        n = 100
+        expected = "84462026"
+        assert.strictEqual(part2(input), expected)
+    });
 
-    // it('Run Phases Repeat Sample 2', () => {
-    //     input = "02935109699940807407585447034323"
-    //     base_pattern = [0, 1, 0, -1]
-    //     n = 100
-    //     expected = "78725270"
-    //     assert.strictEqual(fft(input, base_pattern, n, true, true), expected)
-    // });
+    it('Run Phases Repeat Sample 2', () => {
+        input = "02935109699940807407585447034323"
+        base_pattern = [0, 1, 0, -1]
+        n = 100
+        expected = "78725270"
+        assert.strictEqual(part2(input), expected)
+    });
 
-    // it('Run Phases Repeat Sample 3', () => {
-    //     input = "03081770884921959731165446850517"
-    //     base_pattern = [0, 1, 0, -1]
-    //     n = 100
-    //     expected = "53553731"
-    //     assert.strictEqual(fft(input, base_pattern, n, true, true), expected)
-    // });
+    it('Run Phases Repeat Sample 3', () => {
+        input = "03081770884921959731165446850517"
+        base_pattern = [0, 1, 0, -1]
+        n = 100
+        expected = "53553731"
+        assert.strictEqual(part2(input), expected)
+    });
 })
